6d7a35996f3dc7d8cd0d92a71731775c6f39ba88
Neminda <31001637+Neminda@users.noreply.github.com>
2018-05-03 22:34:17 +0530

    Add files via upload

---
+++ AppContainer.jsx
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, {Component} from 'react';
+import Users from './Users';
+
+export default class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [{
+                id: Date.now(),
+                name: 'John'
+            }]
+        }
+    }
+
+    addUser(user) {
+        this.setState({
+            users: this.state.users.concat({id: Date.now(), name: user.name})
+        })
+    }
+
+    render() {
+        return <div>
+            <h2>Users App</h2>
+            <Users users={this.state.users}/>
+        </div>;
+    }
+}
